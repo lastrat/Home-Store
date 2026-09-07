fcdf3a9a8ec0620302dd210aaf1c51d2229dfa1f
Lastrat <simosimogildas@gmail.com>
2026-09-07 10:41:27 +0100

    fix: import Controller in Api\CartController

--- app/Http/Controllers/Api/CartController.php
+++ app/Http/Controllers/Api/CartController.php
@@ -2,14 +2,13 @@
 
 namespace App\Http\Controllers\Api;
 
+use App\Http\Controllers\Controller;
 use App\Models\Cart;
-use App\Models\CartItem;
 use App\Models\Product;
-use App\Models\Wishlist;
 use App\Models\StockAlert;
+use App\Models\Wishlist;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
 
 class CartController extends Controller
 {
