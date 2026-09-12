01684fd17b9ea7fe10c1263ccb11b9ef42cb8a58
Lastrat <simosimogildas@gmail.com>
2026-09-12 12:12:10 +0100

    fix: disable timestamps on ProductView model
    - Add public \ = false to ProductView
    - Fixes SQL error when inserting into product_views table which uses custom viewed_at instead of standard timestamps

--- app/Models/ProductView.php
+++ app/Models/ProductView.php
@@ -7,6 +7,8 @@ use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class ProductView extends Model
 {
+    public $timestamps = false;
+
     protected $fillable = ['user_id', 'product_id', 'viewed_at'];
 
     protected $casts = [
